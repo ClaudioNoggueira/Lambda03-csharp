b613938c95eb8da12786b42f2d085a87f806fe97
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-01 23:18:02 -0300

    First commit

---
+++ Lambda03/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lambda03.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Lambda03 {
+    class Program {
+        static void Main(string[] args) {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(UpdatePrice);
+            foreach (Product item in list) {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void UpdatePrice(Product p) {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
