b8de1ccb7b0707126bf07a6c92a2dfcdb05650b2
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-01 23:26:56 -0300

    Expressão lambda inline

--- Lambda03/Program.cs
+++ Lambda03/Program.cs
@@ -12,9 +12,7 @@ namespace Lambda03 {
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> action = p => { p.Price += p.Price * 0.1; };
-
-            list.ForEach(action);
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
             foreach (Product item in list) {
                 Console.WriteLine(item);
             }
